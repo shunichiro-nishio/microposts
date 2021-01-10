433b435e50f063581d3aabd30bd3097c9e28f7ce
shunichiro.nishio <shunichiro.nishio@gmail.com>
2021-01-10 04:00:25 +0000

    user login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
 
     /**
      * The priority-sorted list of middleware.
+     * The priority-sorted list of middleware.
      *
      * This forces non-global middleware to always be in the given order.
      *
